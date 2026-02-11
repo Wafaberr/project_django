03c0cdb7c5f1fad52c819b269da102ae15007320
wafa <berrouanewafa@gmail.com>
2026-02-11 11:27:11 +0100

    git commit -m 'registration page and logout link'

--- plateforme/utilisateurs/views.py
+++ plateforme/utilisateurs/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
 def registration_view(request):
-    return render (request,'utilisateurs/registration.html')
+    if request.method=='POST':
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)
+            return redirect('formulaire:liste_fiches')  # Redirigez vers la page d'accueil ou une autre page après l'inscription
+    else:
+        form= UserCreationForm()
+    return render (request,'registration.html', {'form': form})
